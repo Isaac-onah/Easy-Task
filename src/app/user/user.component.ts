8577afb497294206cde2e9677859c7f9f7eec10a
Isaac-onah <iumeha@yahoo.com>
2025-07-25 07:31:01 +0200

    Angular input and output decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randonIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+ @Input({required:true}) id!: string;
+  @Input({required:true}) avatar!: string ;
+  @Input({required:true}) name!: string ;
 
+  @Output() selectUser = new EventEmitter();
+
+  get imagePath() {
+    return 'assets/users/'+ this.avatar;}
+
+
+  onSlectUser() {
+    this.selectUser.emit(this.id)
+  }
 }
